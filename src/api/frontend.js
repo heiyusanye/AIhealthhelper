44791737609dbd370fdb5b737dbc16a75642943d
heiyusanye <2459290393@qq.com>
2026-04-09 02:11:12 +0800

    AI咨询&会话列表

--- src/api/frontend.js
+++ src/api/frontend.js
@@ -3,3 +3,9 @@ import service from '@/utils/request'
 export const register = (data) => {
     return service.post('/user/add', data)
 }
+export const startSession = (data) => {
+    return service.post('/psychological-chat/session/start', data)
+}
+export const getSessionList = (params) => {
+    return service.get('/psychological-chat/sessions', params)
+}
